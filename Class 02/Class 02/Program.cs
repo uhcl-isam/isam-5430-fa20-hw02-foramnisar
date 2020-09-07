6f75afa7a5d28e9c8222f7c82ad110334dd53579
Foram Nisar <f@foramnisar.com>
2020-09-07 18:19:07 -0500

    class 02

---
+++ Class 02/Class 02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter student's grade
+            string gradeInput = Console.ReadLine();
+            int grade = Convert.ToInt32(gradeInput);
+            //display pass if passed
+            if (grade >= 70)
+            {
+                Console.WriteLine("Passed");
+            }
+            // display fail if not passed
+            else
+            //if(!(grade>=70))
+            {
+                Console.WriteLine("Failed");
+            }
+            
+        }
+    }
+}
