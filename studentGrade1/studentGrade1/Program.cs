947ba3cd4e74d309571f587c0858d5abd7fe8355
Foram Nisar <f@foramnisar.com>
2020-09-12 03:07:31 -0500

    Assignment 2

---
+++ studentGrade1/studentGrade1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace studentGrade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter student's score
+            //print the letter grade from the score
+            WriteLine("Enter a score");
+            string gradeInput = ReadLine();
+            int grade = Convert.ToInt32(gradeInput);
+            if (grade >= 90)
+            {
+                WriteLine($"The letter grade of {grade} is A");
+            }
+            else if (grade >= 80)
+            {
+                WriteLine($"The letter grade of {grade} is B");
+            }
+            else if (grade >= 70)
+            {
+                WriteLine($"The letter grade of {grade} is C");
+            }
+            else if (grade >= 60)
+            {
+                WriteLine($"The letter grade of {grade} is D");
+            }
+            else
+            {
+                WriteLine($"The letter grade of {grade} is F");
+            }
+        }
+    }
+}
